5248af02d0dfa4c9289798f763b6da61329db9eb
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-11-25 12:13:11 -0800

    jquery exercise finished

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 $(function() {
-  // YOUR CODE HERE
+  // 1
+  $('#toggleButton').on('click', function() {
+    $("#myParagraph").toggle()
+  })
+  // 2
+  $('#colorButton').on('click', function() {
+    $("#colorDiv").css ({
+      "background-color": "yellow"
+    })
+  })
+  // 3
+  $('#addClassButton').on('click', () => {
+    $('#classDiv').addClass("new-class")
+  })
+  $('#removeClassButton').on('click', () => {
+    $('#classDiv').removeClass("new-class")
+  })
+  // 4
+  $('#fadeInButton').on('click', () => {
+    $('#fadeDiv').fadeIn()
+  })
+  $('#fadeOutButton').on('click', () => {
+    $('#fadeDiv').fadeOut()
+  })
+  // 5
+  $('#slideUpButton').on('click', () => {
+    $('#slideDiv').slideUp()
+  })
+  $('#slideDownButton').on('click', () => {
+    $('#slideDiv').slideDown()
+  })
 })
